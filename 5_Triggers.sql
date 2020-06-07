fb694e3cd9091731e9e5bae81c5f4e4e89b296f4
Nikhil-t01 <nikhiltummidi2002@gmail.com>
2020-06-07 20:59:15 +0530

    added outputs

--- 5_Triggers.sql
+++ 5_Triggers.sql
@@ -1,11 +1,14 @@
 drop table if exists customerordercount;
+-- Query OK, 0 rows affected (0.23 sec)
+
 create table customerordercount (
     customerNumber int not null,
     orderCount bigint default 0,
     primary key (customerNumber),
     foreign key (customerNumber) references customers(customerNumber)
 );
--- Query OK, 0 rows affected (0.40 sec)
+-- Query OK, 0 rows affected (0.35 sec)
+
 
 # Pre-filling data
 insert into customerordercount (customerNumber, orderCount)
@@ -14,7 +17,7 @@ insert into customerordercount (customerNumber, orderCount)
     join orders
     using (customerNumber)
     group by customerNumber;
--- Query OK, 98 rows affected (0.18 sec)
+-- Query OK, 98 rows affected (0.13 sec)
 -- Records: 98  Duplicates: 0  Warnings: 0
 
 describe customerordercount;
@@ -29,10 +32,13 @@ describe customerordercount;
 */
 
 drop trigger if exists after_order_update_orderCount;
+-- Query OK, 0 rows affected, 1 warning (0.00 sec)
+-- Note (Code 1360): Trigger does not exist
+
 create trigger after_order_update_orderCount
     after insert
     on orders for each row
     insert into customerordercount (customerNumber, orderCount)
         values (new.customerNumber, 1)
         on duplicate key update orderCount = orderCount + 1;
--- Query OK, 0 rows affected (0.12 sec)
+-- Query OK, 0 rows affected (0.09 sec)
